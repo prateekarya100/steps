2d9a5255480b3dc1c3a08ed6a91dbb0b90d3ffb9
Þrãtěęk Ærýà <40314403+prateekarya100@users.noreply.github.com>
2025-04-23 15:02:11 +0530

    commit first time in steps react project

--- src/components/Steps.jsx
+++ src/components/Steps.jsx
@@ -3,7 +3,8 @@ import "../index.css";
 
 const Steps = () => {
   const [steps, setSteps] = useState(1);
-  console.log(steps);
+  const [isOpen, setIsOpen] = useState(true);
+  //   console.log(steps);
 
   const messages = [
     "Learn React ⚛️",
@@ -12,34 +13,44 @@ const Steps = () => {
   ];
   return (
     <>
-      <div className="steps">
-        <div className="numbers">
-          <div className={`${steps >= 1 ? "active" : ""}`}>1</div>
-          <div className={`${steps >= 2 ? "active" : ""}`}>2</div>
-          <div className={`${steps >= 3 ? "active" : ""}`}>3</div>
+      <button
+        className="close"
+        onClick={() => {
+          setIsOpen(!isOpen);
+        }}
+      >
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={`${steps >= 1 ? "active" : ""}`}>1</div>
+            <div className={`${steps >= 2 ? "active" : ""}`}>2</div>
+            <div className={`${steps >= 3 ? "active" : ""}`}>3</div>
+          </div>
+          <div className="message">
+            Step {steps} : {messages[steps - 1]}
+          </div>
+          <div className="buttons">
+            <button
+              style={{ background: "#7950f2", color: "white" }}
+              onClick={() => {
+                if (steps >= 2) setSteps(steps - 1);
+              }}
+            >
+              Previous
+            </button>
+            <button
+              style={{ background: "#7950f2", color: "white" }}
+              onClick={() => {
+                if (steps <= 2) setSteps(steps + 1);
+              }}
+            >
+              Next
+            </button>
+          </div>
         </div>
-        <div className="message">
-          Step {steps} : {messages[steps - 1]}
-        </div>
-        <div className="buttons">
-          <button
-            style={{ background: "#7950f2", color: "white" }}
-            onClick={() => {
-              if (steps >= 2) setSteps(steps - 1);
-            }}
-          >
-            Previous
-          </button>
-          <button
-            style={{ background: "#7950f2", color: "white" }}
-            onClick={() => {
-              if (steps <= 2) setSteps(steps + 1);
-            }}
-          >
-            Next
-          </button>
-        </div>
-      </div>
+      )}
     </>
   );
 };
